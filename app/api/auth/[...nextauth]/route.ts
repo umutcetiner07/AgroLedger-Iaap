50655073df7d5a02ad2aaa96908a354bbb8b7a51
Umut Çetiner <umutcetiner@gmail.com>
2026-03-10 20:36:25 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
-import { handler as authHandler } from "@/lib/auth"
-
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
+
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
